6e85ab0a49a0db29061f3e9559fa5e38ccc6539a
Jacob Arychuk <156041293+jzarychuk@users.noreply.github.com>
2024-12-20 10:44:20 -0800

    feat: implement responsive project grid layout

---
+++ react-portfolio/src/components/ProjectGrid.js
@@ -0,0 +1,74 @@
+import './ProjectGrid.css';
+import ProjectCard from './ProjectCard.js';
+
+function ProjectGrid () {
+  return (
+    <div className="project-grid">
+      <ProjectCard 
+        image="/image.jpg"
+        title="My Project"
+        date="December, 2024"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        link="http://www.example.com/"
+      />
+      <ProjectCard 
+        image="/image.jpg"
+        title="My Project"
+        date="December, 2024"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        link="http://www.example.com/"
+      />
+      <ProjectCard 
+        image="/image.jpg"
+        title="My Project"
+        date="December, 2024"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        link="http://www.example.com/"
+      />
+      <ProjectCard 
+        image="/image.jpg"
+        title="My Project"
+        date="December, 2024"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        link="http://www.example.com/"
+      />
+      <ProjectCard 
+        image="/image.jpg"
+        title="My Project"
+        date="December, 2024"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        link="http://www.example.com/"
+      />
+      <ProjectCard 
+        image="/image.jpg"
+        title="My Project"
+        date="December, 2024"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        link="http://www.example.com/"
+      />
+      <ProjectCard 
+        image="/image.jpg"
+        title="My Project"
+        date="December, 2024"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        link="http://www.example.com/"
+      />
+      <ProjectCard 
+        image="/image.jpg"
+        title="My Project"
+        date="December, 2024"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        link="http://www.example.com/"
+      />
+      <ProjectCard 
+        image="/image.jpg"
+        title="My Project"
+        date="December, 2024"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        link="http://www.example.com/"
+      />
+    </div>
+  );
+}
+
+export default ProjectGrid;
