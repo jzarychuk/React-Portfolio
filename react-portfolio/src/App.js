a3edbddc5e3db8b2d4f0e609233ef6afef5f6217
Jacob Arychuk <156041293+jzarychuk@users.noreply.github.com>
2024-12-20 10:45:25 -0800

    feat: implement responsive project grid layout (#8)

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,19 +1,9 @@
 import './App.css';
-import ProjectCard from './components/ProjectCard.js';
+import ProjectGrid from './components/ProjectGrid.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <ProjectCard 
-          image="/image.jpg"
-          title="My Project"
-          date="December, 2024"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
-          link="http://www.example.com/"
-        />
-      </header>
-    </div>
+    <ProjectGrid />
   );
 }
 
