88ed7a5028468846033145d8ca7cabd5b3dbbcc8
Jacob Arychuk <156041293+jzarychuk@users.noreply.github.com>
2024-12-25 12:56:29 -0800

    refactor: move project data handling to App.js and load from config file

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,9 +1,29 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import ProjectGrid from './components/ProjectGrid.js';
 
 function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const response = await fetch('/projects.json');
+        if (!response.ok) {
+          throw new Error('Failed to load projects.json');
+        }
+        const data = await response.json();
+        setProjects(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchProjects();
+  }, []);
+
   return (
-    <ProjectGrid />
+    <ProjectGrid projects={projects} />
   );
 }
 
--- react-portfolio/src/components/ProjectGrid.js
+++ react-portfolio/src/components/ProjectGrid.js
@@ -1,110 +1,20 @@
 import './ProjectGrid.css';
 import ProjectCard from './ProjectCard.js';
 
-// Generate evenly-spaced HSL colors
-const generateColors = (n) => {
-  const step = 360 / n;
-  const colors = [];
-  for (let i = 0; i < n; i++) {
-    const hue = (i * step) % 360;
-    colors.push(`hsl(${hue}, 100%, 40%)`);
-  }
-  return colors;
-};
-
-const tagLabels = [
-  "Tag 1",
-  "Long Tag 2",
-  "Tag 3",
-  "Tag 4",
-  "Tag 5",
-  "Long Tag 6",
-  "Longer Tag 7",
-  "Tag 8",
-  "Tag 9",
-];
-
-function ProjectGrid () {
-  const colors = generateColors(9);
-  const projectTags = tagLabels.map((label, index) => ({
-    label,
-    color: colors[index],
-  }));  
-
+function ProjectGrid ({ projects }) {
   return (
     <div className="project-grid">
-      <ProjectCard 
-        image="/image.jpg"
-        title="My Project"
-        date="December, 2024"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        link="http://www.example.com/"
-        tags={projectTags}
-      />
-      <ProjectCard 
-        image="/image.jpg"
-        title="My Project"
-        date="December, 2024"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        link="http://www.example.com/"
-        tags={projectTags}
-      />
-      <ProjectCard 
-        image="/image.jpg"
-        title="My Project"
-        date="December, 2024"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        link="http://www.example.com/"
-        tags={projectTags}
-      />
-      <ProjectCard 
-        image="/image.jpg"
-        title="My Project"
-        date="December, 2024"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        link="http://www.example.com/"
-        tags={projectTags}
-      />
-      <ProjectCard 
-        image="/image.jpg"
-        title="My Project"
-        date="December, 2024"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        link="http://www.example.com/"
-        tags={projectTags}
-      />
-      <ProjectCard 
-        image="/image.jpg"
-        title="My Project"
-        date="December, 2024"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        link="http://www.example.com/"
-        tags={projectTags}
-      />
-      <ProjectCard 
-        image="/image.jpg"
-        title="My Project"
-        date="December, 2024"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        link="http://www.example.com/"
-        tags={projectTags}
-      />
-      <ProjectCard 
-        image="/image.jpg"
-        title="My Project"
-        date="December, 2024"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        link="http://www.example.com/"
-        tags={projectTags}
-      />
-      <ProjectCard 
-        image="/image.jpg"
-        title="My Project"
-        date="December, 2024"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        link="http://www.example.com/"
-        tags={projectTags}
-      />
+      {projects.map((project) => (
+        <ProjectCard
+          key={project.id}
+          image={project.image}
+          title={project.title}
+          date={project.date}
+          description={project.description}
+          link={project.link}
+          tags={project.tags}
+        />
+      ))}
     </div>
   );
 }
