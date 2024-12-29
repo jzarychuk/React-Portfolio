31dc2121ef62e94abed9dd1fa2dd8111d4704bb2
Jacob Arychuk <156041293+jzarychuk@users.noreply.github.com>
2024-12-29 11:44:26 -0800

    feat: implement tag selection dropdown to filter project cards dynamically

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import ProjectGrid from './components/ProjectGrid.js';
+import TagSortDropdown from './components/TagSortDropdown.js';
 
 function App() {
   const [projects, setProjects] = useState([]);
+  const [selectedTag, setSelectedTag] = useState('');
 
   useEffect(() => {
     const fetchProjects = async () => {
@@ -22,9 +24,26 @@ function App() {
     fetchProjects();
   }, []);
 
+  const allTags = Array.from(
+    new Set(
+      projects.flatMap((project) => project.tags.map((tag) => tag.label)),
+    ),
+  );
+
+  const handleTagSelect = (tag) => {
+    setSelectedTag(tag);
+  };
+
+  const filteredProjects = selectedTag
+    ? projects.filter((project) =>
+        project.tags.some((tag) => tag.label === selectedTag),
+      )
+    : projects;
+
   return (
     <div className="projects-section">
-      <ProjectGrid projects={projects} />
+      <TagSortDropdown tags={allTags} onTagSelect={handleTagSelect} />
+      <ProjectGrid projects={filteredProjects} />
     </div>
   );
 }
